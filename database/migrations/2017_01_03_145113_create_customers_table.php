610094b1b84642520b5842ba274b2b3c8b4b7e69
vagrant <vagrant@homestead>
2017-01-03 15:05:50 +0000

    creazione tabella clienti

---
+++ database/migrations/2017_01_03_145113_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('cid');
+            $table->string('name');
+            $table->string('support_queue');
+            $table->boolean('active');
+            $table->longText('notes')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
